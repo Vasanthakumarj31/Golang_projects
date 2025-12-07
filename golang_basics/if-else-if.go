f0537451f051925fe6dacf6106336684da3342e0
Vasanth <vasanthvasanth4863@gmail.com>
2025-12-07 11:12:05 +0530

    Implement if-else-if structure in Go program

---
+++ golang_basics/if-else-if.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+  x := 9
+  if x>9{
+    fmt.Println("It is greater than 9")
+  }else if x==9{
+    fmt.Println("it is equal to 9")
+  }else{
+    fmt.Println("It is not greater than 9")
+  }
+}
