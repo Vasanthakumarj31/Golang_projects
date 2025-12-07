0661e8125ba39aa9e132090ea2dec90309329f30
Vasanth <vasanthvasanth4863@gmail.com>
2025-12-07 11:12:39 +0530

    Add switch statement to print day of the week

---
+++ golang_basics/switch.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+func main(){
+  day := 2
+  switch day{
+  case 1:
+    fmt.Println("Monday")
+  case 2:
+    fmt.Println("Tuesday")
+  case 3:
+    fmt.Println("Wednesday")
+  case 4:
+    fmt.Println("Thursday")
+  case 5:
+    fmt.Println("Friday")
+  case 6:
+    fmt.Println("Saturday")
+  default:
+    fmt.Println("only choose 1 to 7 numbers")
+  }
+}
